680718ffa9483bfa3061cda02daae0d2e978cd52
Shrikrushna Patil <shri.medisage@gmail.com>
2023-09-30 23:57:24 +0530

    add chat related changes

--- connection/socketServer.js
+++ connection/socketServer.js
@@ -1,36 +1,48 @@
-const socketIo = require('socket.io');
+const socketIo = require("socket.io");
 
 function setupSocketServer(httpServer) {
   const io = socketIo(httpServer, {
     pingTimeout: 60000,
     cors: {
-      origin:'*'
+      origin: "*",
     },
   });
 
-  io.on('connection', (socket) => {
+  io.on("connection", (socket) => {
     console.log("connected to socket.io");
     socket.on("setup", (userData) => {
-        socket.join(userData._id);
-        console.log("User Joined " + userData._id);
-        socket.emit("connected");
+      socket.join(userData._id);
+      console.log("User Joined " + userData._id);
+      socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User joined room : " + room);
+    });
+
+    // socket.on("new message", (message) => {
+    //   console.log("_______________message => " + message.text);
+    // });
+    // In your setupSocketServer.js
+
+    socket.on("new message", (messageData) => {
+      const { senderId, recipientId, text } = messageData;
+      socket.to(recipientId).emit("received message", {
+        senderId,
+        text,
       });
 
-      socket.on("join chat", (room) => {
-        socket.join(room);
-        console.log("User joined room : " + room);
-      });
+      console.log(`Message sent from ${senderId} to ${recipientId}: ${text}`);
+    });
 
-      socket.on("new message", (message) => {
-        console.log("_______________message => " + message);
-      });
-      socket.off("setup", (userData) => {
-        console.log("socket disconnected");
-        socket.leave(userData._id);
-      });    
+    socket.off("setup", (userData) => {
+      console.log("socket disconnected");
+      socket.leave(userData._id);
+    });
   });
 
-  return io; 
+  return io;
 }
 
 module.exports = setupSocketServer;
